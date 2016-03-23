b433c5e0ccd075da5b676577f6efd76a0bc62625
Evdodima <evdodima@gmail.com>
2016-03-23 07:56:56 +0300

    Spring added to homework

--- homework/src/task2016_02_0814/Main.java
+++ homework/src/task2016_02_0814/Main.java
@@ -3,6 +3,8 @@ package task2016_02_0814;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import task2016_02_0814.Tests.*;
 
 import java.lang.*;
@@ -15,18 +17,19 @@ public class Main {
 
 
     public static void main(String[] args) {
+        //testing
+//        Result result = JUnitCore.runClasses(DustCloudTest.class,EarthTypePlanet.class,
+//                GalaxyClusterTest.class, GasCloudTest.class, GiantPlanetTest.class,
+//                GiantStarTest.class,NeutronStarTest.class,PlanetClusterTest.class,
+//                QuasarTest.class,SolarSystemTest.class,
+//                SpiralGalaxyTest.class,SupernovaRemnantTest.class);
+//        for (Failure f:result.getFailures()){
+//            java.lang.System.out.println(f);
+//        }
 
-        Result result = JUnitCore.runClasses(DustCloudTest.class,EarthTypePlanet.class,
-                GalaxyClusterTest.class, GasCloudTest.class, GiantPlanetTest.class,
-                GiantStarTest.class,NeutronStarTest.class,PlanetClusterTest.class,
-                QuasarTest.class,SolarSystemTest.class,
-                SpiralGalaxyTest.class,SupernovaRemnantTest.class);
-        for (Failure f :result.getFailures()){
-            java.lang.System.out.println(f);
-        }
-
-
-
-
+        //main
+        ApplicationContext ac = new ClassPathXmlApplicationContext("/spring-config.xml");
+        Planet p = (Planet) ac.getBean("earth");
+        java.lang.System.out.println(p==null);
     }
 }
