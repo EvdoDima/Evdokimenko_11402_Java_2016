db82e28497255b1cf8af19f213ecae59f02e4552
Evdodima <evdodima@gmail.com>
2016-04-21 19:12:20 +0300

    sem1 project created; templates added

---
+++ SemWork1/src/models/MenuItem.java
@@ -0,0 +1,23 @@
+package models;
+
+/**
+ * Created by evdodima on 11/04/16.
+ * 11-402
+ */
+public class MenuItem {
+    private String name;
+    private String icon;
+
+    public MenuItem( String name, String icon) {
+        this.icon = icon;
+        this.name = name;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ SemWork1/src/models/Page.java
@@ -0,0 +1,14 @@
+package models;
+
+/**
+ * Created by evdodima on 11/04/16.
+ * 11-402
+ */
+public class Page {
+    String name;
+    String content;
+
+    public Page(String name) {
+        this.name=name;
+    }
+}
