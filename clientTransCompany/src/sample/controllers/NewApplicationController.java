27279afadafe49f0574fa136c34b759f2b03a921
Evdodima <evdodima@gmail.com>
2016-05-24 20:27:44 +0300

    NewApplication page with controller added for Customer

---
+++ clientTransCompany/src/sample/controllers/NewApplicationController.java
@@ -0,0 +1,19 @@
+package sample.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by evdodima on 24/05/16.
+ * 11-402
+ */
+public class NewApplicationController {
+    public TextField name;
+    public TextField summ;
+    public TextArea description;
+
+    public void handleSendApplication(ActionEvent actionEvent) {
+
+    }
+}
